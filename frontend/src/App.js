2f24741f308763b12400b0d33c5c5212aed3afb4
srwal <swalsh2024@gmail.com>
2026-02-27 13:15:04 -0500

    Basic welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function SignUpButton() {
+  return (
+    <button className = "SignUpButton"> Sign Up </button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Flourish
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SignUpButton />
       </header>
     </div>
   );
